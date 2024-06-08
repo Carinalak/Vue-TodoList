5a7b0394dbdd71085166467b8dd3ff70f6123849
Carinalak <carina.lakosil@gmail.com>
2024-06-08 17:18:08 +0200

    ✨ first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const addTodo = () => {
+    todo.value.push(new Todo(todoText.value));
+}
+
+const todos = ref<Todo[]>([]);
+const todoText = ref("");
+const handleInput = (e: any) => { 
+    todoText.value = e.target.value
+;}
+
+
+
+
+</script>
+
+<template>
+ 
+
+ <div>
+<input type="text" :value="todoText" @input="handleInput"/>
+<button @click="addTodo">Spara</button>
+
+ </div>
+
+
+</template>
+
+
+
+
+
+
+
+
+<style scoped>
+
+</style>
